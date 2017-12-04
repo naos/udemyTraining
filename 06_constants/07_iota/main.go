0ebf02de1fc9355e4b134fb4fd699a88210c6169
naos <nnonamer@gmail.com>
2017-12-04 21:52:41 +0100

    adds chapters 05 & 06

---
+++ 06_constants/07_iota/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+const (
+	_  = iota             // 0
+	KB = 1 << (iota * 10) // 1 << (1 * 10)
+	MB = 1 << (iota * 10) // 1 << (2 * 10)
+	GB = 1 << (iota * 10) // 1 << (3 * 10)
+	TB = 1 << (iota * 10) // 1 << (3 * 10)
+)
+
+func main() {
+	fmt.Println("binary\t\tdecimal")
+	fmt.Printf("%b\t%d\n", KB, KB)
+	fmt.Printf("%b\t%d\n", MB, MB)
+	fmt.Printf("%b\t%d\n", GB, GB)
+	fmt.Printf("%b\t%d\n", TB, TB)
+}
